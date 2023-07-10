c08c9c19edac79571d74bfe744f6708e05fecfc2
Vanessa Martinez <l.martinezp@uniandes.edu.co>
2023-07-10 15:19:58 -0500

    adv_basics second app

---
+++ flutter_pojects/adv_basics/lib/models/quiz_question.dart
@@ -0,0 +1,12 @@
+class QuizQuestion {
+  const QuizQuestion(this.text, this.answers);
+
+  final String text;
+  final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final shaffledList = List.of(answers);
+    shaffledList.shuffle();
+    return shaffledList;
+  }
+}
---
+++ flutter_pojects/adv_basics/lib/quiz.dart
@@ -0,0 +1,98 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_screen.dart';
+import 'package:adv_basics/results_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+  List<String> selectedAnswers = [];
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  // Otra forma
+
+  // Widget? activeScreen ;
+
+  // @override
+  // void initState() {
+  //   activeScreen = StartScreen(switchScreen);
+  //   super.initState();
+  // }
+
+  // void switchScreen() {
+  //   setState(() {
+  //     activeScreen = const QuestionsScreen();
+  //   });
+  // }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer);
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget =
+          ResultsScreen(chosenAnswers: selectedAnswers, onRestart: restartQuiz);
+    }
+
+    // Otra forma
+    // final screenWidget = activeScreen == 'start-screen'
+    //     ? StartScreen(switchScreen)
+    //     : const QuestionsScreen();
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 0, 95, 167),
+                Color.fromARGB(255, 123, 180, 224),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          // child: activeScreen,
+          // child: activeScreen == 'start-screen'
+          //     ? StartScreen(switchScreen)
+          //     : const QuestionsScreen(),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
